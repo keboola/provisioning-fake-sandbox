836af744d3ef2374ecfea7caacd3a2e5d4a64b29
pivnicek <marc@keboola.com>
2020-04-23 21:34:40 +0200

    redirect all 404s to index

---
+++ src/EventListener/Redirect404.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Keboola\FakeSandbox\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class Redirect404
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        // If not a HttpNotFoundException ignore
+        if (!$event->getThrowable() instanceof NotFoundHttpException) {
+            return;
+        }
+
+        // Create redirect response with url for the home page
+        $response = new RedirectResponse($this->router->generate('index'));
+
+        // Set the response to be processed
+        $event->setResponse($response);
+    }
+}
